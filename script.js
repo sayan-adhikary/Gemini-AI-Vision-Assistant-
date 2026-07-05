7a59f555fad8ba3ef5272690653db2c87f359cdb
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-07-05 16:27:10 +0530

    Update script.js
    Step 4
    Convert Captured Image to Base64

--- script.js
+++ script.js
@@ -6,24 +6,29 @@ const scanBtn = document.getElementById("scan-btn");
 // Start Camera
 async function startCamera() {
     try {
-        // Ask for webcam access
+
+        // Request camera access
         const stream = await navigator.mediaDevices.getUserMedia({
             video: true
         });
 
-        // Show live camera feed
+        // Display camera feed
         video.srcObject = stream;
     } catch (error) {
         console.log("Camera Error:", error);
     }
 }
 
-// Start camera when page loads
+// Start camera automatically
 startCamera();
 
 
-// Capture Image
+// Scan Button
+
 scanBtn.addEventListener("click", captureImage);
+
+
+// Capture Image
 
 function captureImage() {
 
@@ -31,10 +36,10 @@ function captureImage() {
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
 
-    // Get canvas context
+    // Get 2D context
     const ctx = canvas.getContext("2d");
 
-    // Draw current video frame onto canvas
+    // Draw current frame from video
     ctx.drawImage(
         video,
         0,
@@ -44,4 +49,13 @@ function captureImage() {
     );
 
     console.log("Image Captured Successfully!");
+
+    // Convert image into Base64 format
+    const imageData = canvas.toDataURL("image/jpeg");
+
+    // Remove the prefix
+    const base64Image = imageData.split(",")[1];
+
+    console.log(base64Image);
+
 }
