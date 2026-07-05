beb730682b6f4b3afac3d674a9677f6425151a85
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-07-05 16:23:17 +0530

    Update script.js
    ✅ Starting the webcam
    ✅ Capturing the current frame into the hidden canvas

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
+// Select HTML Elements
 const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const scanBtn = document.getElementById("scan-btn");
 
-// Function to start the webcam
+// Start Camera
 async function startCamera() {
     try {
-        // Ask for camera permission
+        // Ask for webcam access
         const stream = await navigator.mediaDevices.getUserMedia({
             video: true
         });
 
-        // Display the live camera feed
+        // Show live camera feed
         video.srcObject = stream;
     } catch (error) {
         console.log("Camera Error:", error);
     }
 }
 
-// Start the camera when the page loads
+// Start camera when page loads
 startCamera();
+
+
+// Capture Image
+scanBtn.addEventListener("click", captureImage);
+
+function captureImage() {
+
+    // Set canvas size equal to video size
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    // Get canvas context
+    const ctx = canvas.getContext("2d");
+
+    // Draw current video frame onto canvas
+    ctx.drawImage(
+        video,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+    console.log("Image Captured Successfully!");
+}
